f9636a3a3cacd8dd1b5577db2df8933b41d13dca
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-12-09 14:53:27 +0100

    jakieś zmiany

--- num6/num6.cpp
+++ num6/num6.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <map>
+
+int ILOSC_WEZLOW = 1024;
 
 using namespace std;
 
@@ -14,7 +17,7 @@ double y_prim(double x) {
 
 vector<double> xn_jednorodne(int n) {
     vector<double> xs;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
         xs.push_back(-1. + ((2.*i) / (n+1.)));
     }
     return xs;
@@ -22,7 +25,7 @@ vector<double> xn_jednorodne(int n) {
 
 vector<double> xn_cos(int n){
     vector<double> xs;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
         xs.push_back(cos( ((2.*i+1.) / (2.*(n+1.))) * M_PI ));
     }
     return xs;
@@ -30,19 +33,28 @@ vector<double> xn_cos(int n){
 
 int main() {
 
-    vector<double> x10 = xn_jednorodne(10);
-    vector<double> x20 = xn_jednorodne(20);
-    vector<double> x40 = xn_jednorodne(40);
-    vector<double> x80 = xn_jednorodne(80);
-    vector<double> x160 = xn_jednorodne(160);
-    vector<double> x320 = xn_jednorodne(320);
-    
-    vector<double> x10_cos = xn_cos(10);
-    vector<double> x20_cos = xn_cos(20);
-    vector<double> x40_cos = xn_cos(40);
-    vector<double> x80_cos = xn_cos(80);
-    vector<double> x160_cos = xn_cos(160);
-    vector<double> x320_cos = xn_cos(320);
+    // Wygenerować jednorodne węzły interpolacji oraz te z cosinusem, np. 1024
+    vector<double> x_jednorodne = xn_jednorodne(ILOSC_WEZLOW);
+    vector<double> x_cosinus = xn_cos(ILOSC_WEZLOW);
+
+    // Dla tych węzłów obliczyć dokładny wynik funkcji y oraz y_prim
+    vector<double> y_jednorodne;
+    vector<double> y_cosinus;
+    vector<double> y_prim_jednorodne;
+    vector<double> y_prim_cosinus;
+
+    for (double x: x_jednorodne) {
+        y_jednorodne.push_back(y(x));
+        y_prim_jednorodne.push_back(y_prim(x));
+    }
+    for (double x: x_cosinus) {
+        y_cosinus.push_back(y(x));
+        y_prim_cosinus.push_back(y_prim(x));
+    }
+
+    // Znaleźć wielomiany różnych stopni, np. 4, 6, 8, 10, 16, 32, ..., 1024
+    // a następnie je wykreślić - skoro wykreślić to znaleźć ich wartości w wielu
+    // punktach?
 
     return 0;
 }
