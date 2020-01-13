d4e267eaa09ed01ea316744cfb8951eb2c757044
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-01-13 18:47:20 +0100

    Skończony Num7

--- num7/NUM7.py
+++ num7/NUM7.py
@@ -3,9 +3,20 @@
 from typing import Sequence, Tuple, Callable
 import numpy as np
 from scipy.linalg import cholesky, solve_triangular
+import matplotlib
 import matplotlib.pyplot as plt
 
-N = 10
+# ustawienia matplotlib dla lepszego wyświetlania w LaTeX.
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
+
+N = 14
 DOKLADNOSC_PRZEDZIALU = 30
 
 
@@ -30,12 +41,12 @@ def wezly_jednorodne() -> Sequence[float]:
 
 
 def wartosci_ksi(yi: Sequence[float]) -> Sequence[float]:
-    A = np.diagflat([4]*(N-2), k=0) +\
-        np.diagflat([1]*(N-3), k=-1) +\
-        np.diagflat([1]*(N-3), k=1)
+    A = np.diagflat([4]*(N-1), k=0) +\
+        np.diagflat([1]*(N-2), k=-1) +\
+        np.diagflat([1]*(N-2), k=1)
     C = cholesky(A, lower=True)
 
-    b = [yi[i] - 2*yi[i+1] + yi[i+2] for i in range(N-2)]
+    b = [yi[i] - 2*yi[i+1] + yi[i+2] for i in range(N-1)]
     h = 2/N
     b = np.multiply(6/h**2, b)
 
@@ -48,24 +59,44 @@ def wartosci_ksi(yi: Sequence[float]) -> Sequence[float]:
 
 
 if __name__ == "__main__":
-    space = np.linspace(-1, 1, num=DOKLADNOSC_PRZEDZIALU*(N-1))
-    # plt.plot(space, list(map(f, space)))
 
+    # przestrzeń liniowa <-1, 1> próbkowana parametrem num
+    linspace = np.linspace(-1, 1, num=DOKLADNOSC_PRZEDZIALU*(N))
+
+
+    # ustawienie wykresu
+    plt.figure(figsize=(5.5, 3.5))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.suptitle('Naturalny splajn kubiczny')
+    plt.title('N = {}'.format(N))
+
+    # rysowanie funkcji f
+    plt.plot(linspace, list(map(f, linspace)), 'r--')
+
+
+    # punkty sklejania
     xi = wezly_jednorodne()
     yi = [f(x) for x in xi]
-    war_ksi = wartosci_ksi(yi)
 
-    for nr_przedzialu in range(N-1):
-        x_0 = xi[nr_przedzialu]
-        x_1 = xi[nr_przedzialu+1]
-        y_0 = yi[nr_przedzialu]
-        y_1 = yi[nr_przedzialu+1]
-        ksi_0 = war_ksi[nr_przedzialu]
-        ksi_1 = war_ksi[nr_przedzialu+1]
+    # drugie pochodne wielomianów
+    Ksi = wartosci_ksi(yi)
+
+    # rysowanie wielomianów w przedziałach
+    for pkt_sklejenia in range(N):
+        x_0 = xi[pkt_sklejenia]
+        x_1 = xi[pkt_sklejenia+1]
+        y_0 = yi[pkt_sklejenia]
+        y_1 = yi[pkt_sklejenia+1]
+        ksi_0 = Ksi[pkt_sklejenia]
+        ksi_1 = Ksi[pkt_sklejenia+1]
+
         wielomian = s(x_0, x_1, y_0, y_1, ksi_0, ksi_1)
-        przedzial = np.linspace(x_0, x_1, DOKLADNOSC_PRZEDZIALU)
+        przedzial = np.linspace(x_0, x_1, num=DOKLADNOSC_PRZEDZIALU)
 
         plt.plot(przedzial, list(map(wielomian, przedzial)))
 
+    # plt.show()
+    filename = '_'.join(['spline', 'N', str(N)])
+    plt.savefig(filename + '.pgf')
 
-    plt.show()
